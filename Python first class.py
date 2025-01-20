b45733f374823213154cfbd774b56940e637e54a
vishnuchappidi12 <vishnuchappidi12@gmail.com>
2025-01-20 18:09:19 +0530

    Add files via upload

---
+++ Python first class.py
@@ -0,0 +1,54 @@
+--------------------------------------------------------------------
+python is a general purpose high level interprited, dynamically typed language.
+--------------------------------------------------------------------
+-----features of python---------
+-------------------------------------------------------------------
+It is a simple language, easy to learn without having any programmimng background.
+statements return in python was nearer to english language.so it is a highlevel language.
+genereal purpose programming language.
+we can build any applications for any domain using python.
+-------------------------------------------------------------------
+-----Dynamically typed language-------
+--------------------------------------------------------------------
+At the time of defining variable no need to specify data type,based in the data we are assigning type of variable will decide at run time.
+--------------------------------------------------------------------
+-----Interprirted language-------
+--------------------------------------------------------------------
+Translate line by line,debug easily.
+To improve the performance of interprited JIT complier was added.
+--------------------------------------------------------------------
+-----Protable and platfrom Independent------
+--------------------------------------------------------------------
+We can run the applications return in one platfrom iun other platfrom without doing any modifications.
+--------------------------------------------------------------------
+--------Archietcture Netrual-----------
+--------------------------------------------------------------------
+The behaviour of Python will remains same on any system archiecture.
+Python supports both oriented and object oreinted concepts.
+python provides rich set of libraries to build applications in different domains.
+python provides garbage collection to deallocate alloctaed memory.	
+--------------------------------------------------------------------
+------literals or constants-------
+1.numerical 
+eg: 35,285,-120
+2.real or float
+eg:3.4,-2.8
+3.complex 
+eg:3+4j,4-2j
+4.Boolean 
+eg: true, false
+5.String 
+Any thing returns in either single code or double code are string constants.
+eg:"hello",'hi'.
+6.special 
+None(value nothing)
+-------------------------------------------------------------------------
+----Identifiers---
+all user defeined name are identifiers, name given to variable or object,class, function.
+
+----Rules for identifiers-----
+identifiers starts with either alphabet or underscore.
+After trhe first letter subsquent letters may be either alphabet or numbers or underscore.
+key words are not allowed, space are not allowed,special symbols are not allowed expect underscore.
+python is a case sensetive language, there was a difference between lowercase and uppercase alphabets.
+--------------------------------------------------------------------------
