f3ca4d96ff1ef281058fb57a57d7723be9fd0d15
vishnuchappidi12 <vishnuchappidi12@gmail.com>
2025-01-21 17:45:31 +0530

    Add files via upload

---
+++ second class _python.py
@@ -0,0 +1,48 @@
+--------------------------------------------------------------
+----Variable-------
+-------------------------------------------------------------
+1.IT'S value can var so it is a variable
+     (or)
+2.Named memory locations are called variables.
+3.Python variables are immutable.
+------syntax for defining a variable-------
+Var name=value
+--------------------------------------------------------------
+-----Key wors-----
+--------------------------------------------------------------
+1.resereved words are keywords
+functionality of these words already defined in a translate we need to use for the same functionality.
+eg:import keyword
+key word,kwlist.
+--------------------------------------------------------------
+------opeartors-------
+-------------------------------------------------------------- 
+1.Arithmetic operator
+(+,-,*,/,%,**,//)
+--------------------------
+2.Relational operator
+eg: <,>,<=,>=,==,!=
+--------------------------
+3.logical operator
+eg:And,Or,Not
+--------------------------
+4.Assignment operator
+eg: =
+--------------------------
+5.swapping of two numbers
+a=5
+b=8
+a,b=b,a
+a=8
+b=5
+--------------------------
+6.Short hand property
+Eg:a=a+b this can written as a+=b
+--------------------------
+7.unary operator
+To represnt negative sign
+--------------------------
+8.Bitwise opeartot
+Eg:28 --11100
+eg:23 --10111
+------------------------------------------------------------------------    
