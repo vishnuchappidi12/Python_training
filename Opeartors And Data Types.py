e609c019aa0c832a25c2aa415611960c9f986737
vishnuchappidi12 <vishnuchappidi12@gmail.com>
2025-01-22 19:44:33 +0530

    Add files via upload

---
+++ Opeartors And Data Types.py
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------
+-----Left Shift-------
+n<<b then result is n*2^b
+       (or)
+Add b zeros at lsb side
+-----------------------------------------------------------------------
+-----Right shift-------
+if n>>b then n//2^b 
+       (or)
+Discard b bits from lsb side
+------------------------------------------------------------------------
+------Complement------
+~n is -(n+1)
+eg: n=20 =>-(20+1) =>-21
+------------------------------------------------------------------------
+------Member ship operator------
+To know given element is available within a sequence or not
+-------------------------------------------------------------------------
+----Idenity opeartor------
+To verify two objects or variable are reffering same memory refernce or not
+-------------------------------------------------------------------------
+--------Data Tyes------
+**Integer:
+If we assign integer constant to a variable its type will become int.
+----------------------------------------------------------------------------
+------Float Data type----
+* If we assign real constant to a variable
+---------------------------------------------------------------------------
+-----Complext data Type-----
+*a=3+4j
+Its type shows complex
+-----boolean Data Type-----
+*a=true
+its type shows Boolean
+-----None type----
+*If we assign none to the variable items a=none
+------sequence data type------
+1.List =>List is a collection of collection it may be homogenous or hetrogenous elements.
+=>List maintain insertion order
+=>list is muttable
+=>list supports both positive and negative indexing
+----------------------------------
+2.Tuple
+=>Tuple is a collection of elements it is muttable.
+-----------------------------------
+3.Strin 
+=>String id a collection of character enclosed within the quotes
+=>string is immuatble
+-----------------------------------
+4.Range
+=>It is used to generate a sequence of values
+=>It is immutable
+----------------------------------------------------------------------------------
