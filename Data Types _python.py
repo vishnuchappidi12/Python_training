b9ba2bf2fe6ea8a278e5b7d9d451d206e0d5e66a
vishnuchappidi12 <vishnuchappidi12@gmail.com>
2025-01-23 21:59:03 +0530

    Add files via upload

---
+++ Data Types _python.py
@@ -0,0 +1,11 @@
+------------------------------------------------------------------
+----Maping data type dictionaty------
+*Dictionary is a collection of key ,value pair
+*Dictionary was mutable,key should be unique , any immutable data type.
+*duplicates keys are not allowed.
+*Dictionary doesnt maintain insertion order
+-------------------------------------------------------------------
+------------Type Conversion------------
+*Converting one datavtype into another datatype is called the type conversion
+Note: Python doesnt type implict type convesrion
+------------------------------------------------------------------
